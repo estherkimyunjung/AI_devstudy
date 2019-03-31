73aeafcbbc0a758f045607fb416db9f59af34398
estherkimyunjung <48709512+estherkimyunjung@users.noreply.github.com>
2019-03-31 09:45:23 +0900

    Create SelfStudyFirst.java
    Test editplus program and create first java file.

---
+++ Java/selfstudy/SelfStudyFirst.java
@@ -0,0 +1,5 @@
+class SelfStudyFirst {								//class name must match with file name
+	public static void main(String[] args) {	
+		System.out.println("Hello Java World~!!");	//Print String in window screen
+	}
+}
